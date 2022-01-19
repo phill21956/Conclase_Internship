f968791f7ab97078d73cad6bdef3c7d8284c37d6
philip inegbedion <phillipinegbedion@gmail.com>
2022-01-19 12:35:00 +0100

    updated

---
+++ control_flow.dart
@@ -0,0 +1,20 @@
+void main(){
+int score = 101;
+
+  if(score >= 70 && score <=100){
+    print("You got an A!!");
+  }else if(score >=60 && score <=69)
+    print('You got a B!!');
+    else if(score >=50 && score <=59)
+    print('You got a C!!');
+    else if(score >=40 && score <=49)
+    print('You got a D!!');
+    else if(score >=30 && score <=39)
+    print('You got a E!!');
+    else if(score >=0 && score <=29)
+    print('You got a F!!');
+    else{
+      print('Not in range');
+    }
+  
+  }
